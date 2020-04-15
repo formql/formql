7838546ae5248ef3a0aa2191e39e1b78c83ae831
Juan Stoppa <jstoppa@gmail.com>
2020-04-15 22:52:56 +0100

    adding scripts

--- packages/core/src/formql.module.ts
+++ packages/core/src/formql.module.ts
@@ -87,7 +87,7 @@ export class FormQLModule {
       FormQLTextareaComponent,
       FormQLSelectComponent,
       FormQLRadioComponent,
-      FormQLCheckboxComponent      
+      FormQLCheckboxComponent
     );
-  }    
+  }
 }
